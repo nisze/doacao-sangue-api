73a0c0f53827fec6a5d532c1fc65a9d8ccc72776
Denise Korgiski de Oliveira <dkorgiski@gmail.com>
2025-03-30 19:54:11 -0300

    Initial commit

---
+++ src/main/java/com/exemplo/doacaosangue/DoacaoSangueApiApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.doacaosangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoacaoSangueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoacaoSangueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/doacaosangue/DoacaoSangueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.doacaosangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoacaoSangueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
